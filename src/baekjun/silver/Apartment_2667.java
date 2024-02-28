92939dfa9605b9b238383d71635885d2d21254d5
hikhjin <hikhjin@gmail.com>
2024-02-28 23:55:28 +0900

    2667

---
+++ src/baekjun/silver/Apartment_2667.java
@@ -0,0 +1,22 @@
+package baekjun.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Apartment_2667 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(br.readLine()); //총 단지 수
+
+        for (int i = 0; i < N; i++) {
+
+        }
+
+
+    }
+}
