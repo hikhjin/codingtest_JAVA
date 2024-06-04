2e4852d15d2d0feded91daa4711c3b4619cba196
hikhjin <hikhjin@gmail.com>
2024-06-04 12:42:40 +0900

    240604

---
+++ src/baekjun/silver/Josephus_1158_2.java
@@ -0,0 +1,34 @@
+package baekjun.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Josephus_1158_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        List<Integer> list = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+
+        int idx = 0;
+        sb.append("<");
+        while (list.size() > 1) {
+            idx = (idx + K - 1) % list.size();
+            sb.append(list.remove(idx)).append(", ");
+        }
+        sb.append(list.remove(0)).append(">");
+
+        System.out.println(sb);
+
+    }
+}
