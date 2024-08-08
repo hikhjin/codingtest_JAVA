4a9b0da25e49cecae7fa65f3eaed469b26674cf8
hikhjin <hikhjin@gmail.com>
2024-08-08 20:01:02 +0900

    240808

--- src/baekjun/silver/EatOrAte_7795.java
+++ src/baekjun/silver/EatOrAte_7795.java
@@ -1,8 +1,6 @@
 package baekjun.silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
@@ -10,6 +8,7 @@ import java.util.StringTokenizer;
 public class EatOrAte_7795 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
 
@@ -45,7 +44,9 @@ public class EatOrAte_7795 {
                     }
                 }
             }
-            System.out.println(cnt);
+            bw.write(cnt + "\n");
         }
+        bw.flush();
+        bw.close();
     }
 }
