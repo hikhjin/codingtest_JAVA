6db2b64b5b5cf2d932f76f551428115092285d25
hikhjin <hikhjin@gmail.com>
2024-12-09 15:59:36 +0900

    241209

--- src/baekjun/silver/MeetingRooms2_1931.java
+++ src/baekjun/silver/MeetingRooms2_1931.java
@@ -1,4 +1,37 @@
 package baekjun.silver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class MeetingRooms2_1931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1] == o2[1]) return o1[0] - o2[0]; // 끝나는 시간 같으면 시작 시간으로 오름차순 정렬
+            return o1[1] - o2[1];
+        });
+
+        int cnt = 0;
+        int end = 0;
+        for (int[] meeting : arr) {
+            if (meeting[0] >= end) {
+                end = meeting[1];
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
